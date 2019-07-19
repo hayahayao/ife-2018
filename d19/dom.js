8291d9f1c289236a7545e9f1e5c7b133e81c4728
hayahayao <azurecho13@gmail.com>
2019-07-19 22:06:46 +0800

    d19

---
+++ d19/dom.js
@@ -0,0 +1,46 @@
+function getAllListItem() {
+    // 返回页面中所有li标签
+    return document.getElementsByTagName('li');
+}
+
+function findAllHtmlSpanInOneSection(sectionId) {
+    // 返回某个section下所有span中内容为HTML的span标签
+    var spans = document.getElementsByTagName('span');
+    var res = [];
+    for (var i = 0; i < spans.length; i++) {
+        if (spans[i].innerHTML === 'HTML' && spans[i].parentNode.parentNode.parentNode.parentNode.id === sectionId) {
+            res.push(spans[i]);
+        }
+    }
+    return res;
+}
+
+function findListItem(sectionId, spanCont) {
+    // 返回某个section下，所有所包含span内容为spanCont的LI标签
+    var spans = document.getElementsByTagName('span');
+    var res = [];
+    for (var i = 0; i < spans.length; i++) {
+        if (spans[i].innerHTML === spanCont && spans[i].parentNode.parentNode.parentNode.parentNode.id === sectionId) {
+            res.push(spans[i].parentNode);
+        }
+    }
+    return res;
+}
+
+function getActiveLinkContent(sectionId) {
+    // 返回某个section下，class为active的链接中包含的文字内容
+    var nodes = document.getElementById(sectionId).children[1].children[0].children;
+    var res = '';
+    for (var i = 0; i < nodes.length; i++) {
+        if (nodes[i].children[1].className === 'active') {
+            res = nodes[i].children[1].innerHTML;
+        }
+    }
+    return res;
+}
+
+var items = getActiveLinkContent('news-top');
+console.log(items);
+// for (var i = 0; i < items.length; i++) {
+//     items[i].style.backgroundColor = 'red';
+// }

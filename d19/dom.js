43f60bf68f7bb01f4a3ce2f8ec89947082c1d621
hayahayao <azurecho13@gmail.com>
2019-07-19 22:20:21 +0800

    d19: querySelector

--- d19/dom.js
+++ d19/dom.js
@@ -1,14 +1,27 @@
 function getAllListItem() {
     // 返回页面中所有li标签
-    return document.getElementsByTagName('li');
+
+    // return document.getElementsByTagName('li');
+    
+    return document.querySelectorAll('li');
 }
 
 function findAllHtmlSpanInOneSection(sectionId) {
     // 返回某个section下所有span中内容为HTML的span标签
-    var spans = document.getElementsByTagName('span');
+    
+    // var spans = document.getElementsByTagName('span');
+    // var res = [];
+    // for (var i = 0; i < spans.length; i++) {
+    //     if (spans[i].innerHTML === 'HTML' && spans[i].parentNode.parentNode.parentNode.parentNode.id === sectionId) {
+    //         res.push(spans[i]);
+    //     }
+    // }
+    // return res;
+
+    var spans = document.querySelectorAll('#'+sectionId+' span');
     var res = [];
     for (var i = 0; i < spans.length; i++) {
-        if (spans[i].innerHTML === 'HTML' && spans[i].parentNode.parentNode.parentNode.parentNode.id === sectionId) {
+        if (spans[i].innerHTML === 'HTML') {
             res.push(spans[i]);
         }
     }
@@ -17,10 +30,20 @@ function findAllHtmlSpanInOneSection(sectionId) {
 
 function findListItem(sectionId, spanCont) {
     // 返回某个section下，所有所包含span内容为spanCont的LI标签
-    var spans = document.getElementsByTagName('span');
+
+    // var spans = document.getElementsByTagName('span');
+    // var res = [];
+    // for (var i = 0; i < spans.length; i++) {
+    //     if (spans[i].innerHTML === spanCont && spans[i].parentNode.parentNode.parentNode.parentNode.id === sectionId) {
+    //         res.push(spans[i].parentNode);
+    //     }
+    // }
+    // return res;
+
+    var spans = document.querySelectorAll('#'+sectionId+' span');
     var res = [];
     for (var i = 0; i < spans.length; i++) {
-        if (spans[i].innerHTML === spanCont && spans[i].parentNode.parentNode.parentNode.parentNode.id === sectionId) {
+        if (spans[i].innerHTML === spanCont) {
             res.push(spans[i].parentNode);
         }
     }
@@ -29,17 +52,21 @@ function findListItem(sectionId, spanCont) {
 
 function getActiveLinkContent(sectionId) {
     // 返回某个section下，class为active的链接中包含的文字内容
-    var nodes = document.getElementById(sectionId).children[1].children[0].children;
-    var res = '';
-    for (var i = 0; i < nodes.length; i++) {
-        if (nodes[i].children[1].className === 'active') {
-            res = nodes[i].children[1].innerHTML;
-        }
-    }
-    return res;
+    
+    // var nodes = document.getElementById(sectionId).children[1].children[0].children;
+    // var res = '';
+    // for (var i = 0; i < nodes.length; i++) {
+    //     if (nodes[i].children[1].className === 'active') {
+    //         res = nodes[i].children[1].innerHTML;
+    //     }
+    // }
+    // return res;
+
+    var node = document.querySelector('#'+sectionId+' .active');
+    return node.innerHTML;
 }
 
-var items = getActiveLinkContent('news-top');
+var items = getActiveLinkContent('news-normal');
 console.log(items);
 // for (var i = 0; i < items.length; i++) {
 //     items[i].style.backgroundColor = 'red';

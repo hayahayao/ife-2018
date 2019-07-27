f372910e834d92fcad7acd55c4357afb77eba820
hayahayao <azurecho13@gmail.com>
2019-07-27 16:24:36 +0800

    d28-30: part2-4

--- d28-30/main.js
+++ d28-30/main.js
@@ -3,18 +3,44 @@ document.getElementById('email-input').onkeyup = function () {
 	displayContent();
 }
 
+document.getElementById('email-sug-wrapper').onclick = function (event) {
+	if (event.target.tagName.toLowerCase() === 'li') {
+		document.getElementById('email-input').value = event.target.innerHTML;
+		undisplay();
+	}
+}
+
 function getInput () {
 	return document.getElementById('email-input').value.trim();
 }
 
 function generateContent () {
 	var input = getInput();
+	var pos = input.indexOf('@');
+	var prefix = '',
+		suffix = '';
+	if (pos > -1) {
+		prefix = input.slice(0, pos);
+		suffix = (input.length > pos + 1) ? input.slice(pos + 1, input.length) : '';
+	} else {
+		prefix = input;
+		suffix = input;
+	}
 	var postfixList = ['163.com', 'gmail.com', '126.com', 'qq.com', '263.net'];
 	var content = [];
 	for (var i = 0; i < postfixList.length; i++) {
-		var li = document.createElement('li');
-		li.innerHTML = input + '.' + postfixList[i];
-		content.push(li);
+		if (postfixList[i].indexOf(suffix) === 0) {
+			var li = document.createElement('li');
+			li.innerHTML = prefix + '@' + postfixList[i];
+			content.push(li);
+		}
+	}
+	if (content.length === 0) {
+		for (var i = 0; i < postfixList.length; i++) {
+			var li = document.createElement('li');
+			li.innerHTML = prefix + '@' + postfixList[i];
+			content.push(li);
+		}
 	}
 	return content;
 }

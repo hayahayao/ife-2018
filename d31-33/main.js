0c83a939231d4e2b2e5c30bbcdbb76ceaf823b95
hayahayao <azurecho13@gmail.com>
2019-08-03 19:56:13 +0800

    d31-33: part3

--- d31-33/main.js
+++ d31-33/main.js
@@ -1,21 +1,67 @@
 window.onload = function () {
-	updateTableList(getDataList());
-	document.getElementById('region-select').onchange = function () {
-		updateTableList(getDataList());
-	};
-	document.getElementById('product-select').onchange = function () {
-		updateTableList(getDataList());
-	};
-};
+	generateCheckbox(document.getElementById('region-radio-wrapper'), 'region', ['华东', '华北', '华南']);
+	generateCheckbox(document.getElementById('product-radio-wrapper'), 'product', ['手机', '笔记本', '智能音箱']);
+}
 
-function getDataList () {
-	let select1 = document.getElementById('region-select');
-	let option1 = select1.options[select1.selectedIndex].innerHTML;
-	let select2 = document.getElementById('product-select');
-	let option2 = select2.options[select2.selectedIndex].innerHTML;
+// 生成一组checkbox
+function generateCheckbox (wrapper, type, arr) {
+	let checkbox = '<input type="checkbox" name="' + type + '" value="all" checkbox-type="all">全选';
+	for (let i = 0; i < arr.length; i++) {
+		checkbox += '<input type="checkbox" name="' + type + '" value="' + arr[i] + '" checkbox-type="sub">' + arr[i];
+	}
+	wrapper.innerHTML = checkbox;
+
+	wrapper.onclick = function (event) {
+		if (event.target.type === 'checkbox') {
+			if (event.target.getAttribute('checkbox-type') === 'all') {
+				for (let i = 0; i < wrapper.children.length; i++) {
+					wrapper.children[i].checked = true;
+				}
+			} else if (event.target.getAttribute('checkbox-type') === 'sub') {
+				if (!event.target.checked) { 
+					let cnt = 0;
+					for (let i = 0; i < wrapper.children.length; i++) {
+						if (wrapper.children[i].checked) {
+							cnt++;
+						}
+					}
+					if (cnt === 0) { // 不允许一个都不勾选，所以当用户想取消唯一一个被勾选的子选项时，无交互反应，不允许取消勾选
+						event.target.checked = true;
+					} else if (cnt === wrapper.children.length - 1) { // 如果当前是全选状态，取消任何一个子选项，则全选CheckBox也要置为未勾选状态
+						wrapper.children[0].checked = false;
+					}
+				} else { // 点击最后一个未被选中的单个选项后，全选CheckBox也要置为被勾选状态
+					let cnt = 0;
+					for (let i = 0; i < wrapper.children.length; i++) {
+						if (wrapper.children[i].checked && wrapper.children[i].getAttribute('checkbox-type') === 'sub') {
+							cnt++;
+						}
+					}
+					if (cnt === wrapper.children.length - 1) {
+						wrapper.children[0].checked = true;
+					}
+				}
+			}
+		}
+		updateTableList(getData());
+	}
+}
+
+function getData () {
+	let checkboxs = document.getElementsByTagName('input');
+	let regions = [];
+	let products = [];
+	for (let i = 0; i < checkboxs.length; i++) {
+		if (checkboxs[i].name === 'region' && checkboxs[i].checked && checkboxs[i].getAttribute('checkbox-type') === 'sub') {
+			regions.push(checkboxs[i].value);
+		}
+		if (checkboxs[i].name === 'product' && checkboxs[i].checked && checkboxs[i].getAttribute('checkbox-type') === 'sub') {
+			products.push(checkboxs[i].value);
+		}
+	}
 	let res = [];
 	for (let i = 0; i < sourceData.length; i++) {
-		if (sourceData[i].region === option1 && sourceData[i].product === option2) {
+		if (regions.includes(sourceData[i].region) && products.includes(sourceData[i].product)) {
 			res.push(sourceData[i]);
 		}
 	}
